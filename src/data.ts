9e1356da067b5b864741b3acddb391200a9b5bee
Jyotirmoy Barman <jyotirmoydotdev@gmail.com>
2025-01-08 01:45:39 +0530

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -197,10 +197,4 @@ export const blogs: blogsType[] = [
     },
 ]
 
-export const video: {title: string, date: string, embedUrl: string}[] = [
-  {
-    "title": "1408. String Matching in an Array",
-    "date": "2025-01-07T17:41:43Z",
-    "embedUrl": "https://www.youtube.com/embed/7_dOHPE-6lo"
-  }
-]
+export const video: {title: string, date: string, embedUrl: string}[] = []
