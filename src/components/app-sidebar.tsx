f4fe2486f24fe8cf85e4fdffedd6c50a4d5e7e04
Jyotirmoy Barman <jyotirmoydotdev@gmail.com>
2025-01-29 23:45:11 +0530

    chore: update logo link

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -33,7 +33,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
     return (
         <Sidebar collapsible="icon" {...props}>
             <SidebarHeader>
-                <Link href={"/dashboard"} className="flex gap-2 items-center h-8">
+                <Link href={"/"} className="flex gap-2 items-center h-8">
                     <Image src={'/jyotirmoy.webp'} width={100} height={100} alt="" className="flex aspect-square size-7 items-center justify-center rounded-lg object-contain text-sidebar-primary-foreground" />
                     <span className={`truncate font-semibold text-xl leading-tight text-black dark:text-white pl-1 pr-5 ${baloo.className}`}> {/**bg-[rgb(237,109,86)] text-white dark:bg-inherit dark:text-[rgb(237,109,86)] */}
                         Jyotirmoy.dev
